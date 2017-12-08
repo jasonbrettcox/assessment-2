f34bd48399b032070e6709994ac2e8acd5687b5d
jasonbcox <jasonbcox@gmail.com>
2017-12-08 00:06:22 -0700

    buttons are up, not working yet

--- main.js
+++ main.js
@@ -3,4 +3,21 @@
   integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
   crossorigin="anonymous"></script>
 
+  <script>
+  $(document).keydown(function(e) {
+  	var keycode = e.keyCode || e.which,
+  	arrow = {left: 65, right: 76 };
+	
+	switch (keyCode) {
+
+		case arrow.left:
+			$('#block1').animate({"right": "+=75px" }, "slow");
+			break;
+
+		case arrow.right:
+			$('#block2').animate({"right": "+=75px" }, "slow");
+			break
+		}
+	});	
+});
   
