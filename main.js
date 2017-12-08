683b8593885d8f88e6e58a0c0264c661ff1cdcdc
jasonbcox <jasonbcox@gmail.com>
2017-12-08 08:54:47 -0700

    working now

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
+var arr1 = 0;
+var arr2 = 0;
 
-  $(document).keydown(function(run) {
-  	if(run.keyCode == 90)
+  
+
+  $(document).keydown(function(q) {
+  	if(q.keyCode == 90)
   		$('.block1').animate({marginLeft: '+=5%' })
-  		console.log('.block1')
-  		else if(run.keyCode == 191)
+  		arr1 += 1;
+  		if ( arr1 === 80) {alert("playa 1 wins" );}
+ 
+  	if(q.keyCode == 191)
   		$('.block2').animate({marginLeft: '+=5%' })
-  		console.log('.block2')
-  });
+  		arr2 += 1;
+  		if ( arr2 === 80) {alert("playa 2 wins" );}
+
+});
+ 
+  
   	
-	};
+	
 
 
 // 		case arrow.left:
